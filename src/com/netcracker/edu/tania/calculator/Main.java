b1945d92f8d3d545a2e756e1bba442419985b10d
TanyaKolp <lancerchg@gmail.com>
2016-11-07 16:19:00 +0300

    second

--- src/com/netcracker/edu/tania/calculator/Main.java
+++ src/com/netcracker/edu/tania/calculator/Main.java
@@ -3,10 +3,12 @@ package com.netcracker.edu.tania.calculator;
 
 /**
  * Created by tania on 7/18/16.
+ *
  */
 public class Main {
 
     public static void main(String[] args) {
+
         ControllerInput contr = new ControllerInput();
         contr.init();
 
